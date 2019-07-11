42113196e766c36ef603cb1a3404c50a4d93b2e9
Marcelo Cueto <marcelo.cueto@uchile.cl>
2019-07-11 17:12:15 -0400

    Add multiples functions & calls on main function
    The values returned by `five` & `twenty` functions are printed by function print_value

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 fn main() {
-    print_value(5);
+    let new_value = sum_values(five(), twenty());
+    print_value(new_value);
 }
 
 fn print_value(x: i32) {
     println!("The value of x is: {}", x);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn twenty() -> i32 {
+    return 20;
+}
+
+fn sum_values(value_1: i32, value_2: i32) -> i32 {
+    return value_1 + value_2;
 }
