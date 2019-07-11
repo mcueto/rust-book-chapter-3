76b3639b980aa88f3d5a86dab197afffc2849308
Marcelo Cueto <marcelo.cueto@uchile.cl>
2019-07-11 17:39:53 -0400

    Set value var & evaluate `if` statement on main function

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
 fn main() {
-    let new_value = sum_values(five(), twenty());
-    print_value(new_value);
-}
+    let number = 3;
 
-fn print_value(x: i32) {
-    println!("The value of x is: {}", x);
-}
+    if number < 5 {
+        println!("condition was true");
 
-fn five() -> i32 {
-    5
-}
-
-fn twenty() -> i32 {
-    return 20;
-}
-
-fn sum_values(value_1: i32, value_2: i32) -> i32 {
-    return value_1 + value_2;
+    } else {
+        println!("condition was false");
+    }
 }
