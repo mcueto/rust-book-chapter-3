c18f2ce9239f2aa9d3d81334b52cf25624fca820
Marcelo Cueto <marcelo.cueto@uchile.cl>
2019-07-11 16:17:27 -0400

    Print tuple item by index on main function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 fn main() {
     let tup: (i32, f64, u8) = (500, 6.4, 1);
 
-    // println!("This tuple has data: {}", tup); // this doesnt work
-
-    // this works
-    let (x, y, z) = tup;
-    println!("This x on tuple has data: {}", x);
+    println!("The second data in tup is: {}", tup.1);
 }
