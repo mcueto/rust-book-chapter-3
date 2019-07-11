f9dc15fdad1571e95543c36b08517085468d32c5
Marcelo Cueto <marcelo.cueto@uchile.cl>
2019-07-11 17:47:34 -0400

    Add if condition inside a let

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 fn main() {
-    let number = 3;
-
-    if number % 4 == 0 {
-        println!("number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("number is divisible by 3");
-    } else if number % 2 == 0 {
-        println!("number is divisible by 2");
+    let condition = true;
+    let number = if condition {
+        5
     } else {
-        println!("number is not divisible by 4, 3, or 2");
-    }
+        6
+    };
+
+    println!("The value of number is: {}", number);
 }
